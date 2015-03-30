942a31e534843b6953fd6d565974d4d82b49a007
Louise Beh <louise.beh@gmail.com>
2015-03-30 18:38:46 +0100

    Added validation for restaurant uniqueness

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -70,5 +70,24 @@ feature 'restaurants' do
     end
   end
 
+  context 'creating restaurants' do
 
+    context 'an invalid restaurant' do
+      it 'does not let you submit a name that is too short' do
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'kf'
+        click_button 'Create Restaurant'
+        expect(page).not_to have_css 'h2', text: 'kf'
+        expect(page).to have_content 'error'
+      end
+
+      it "is not valid unless it has a unique name" do
+        Restaurant.create(name: "Moe's Tavern")
+        restaurant = Restaurant.new(name: "Moe's Tavern")
+        expect(restaurant).to have(1).error_on(:name)
+      end
+    end
+
+  end
 end
